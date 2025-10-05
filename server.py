8833a745c9ede2a946a8cabbaa7c7816bf7de8ca
sgarrow <stangarrow@yahoo.com>
2025-10-04 18:51:39 -0700

    'v3.21.21'. Commonize spiClock and sprinkler2 - part 4.

--- server.py
+++ server.py
@@ -8,6 +8,33 @@ import cmdVectors      as cv # Contains vectors to "worker" functions.
 import cfg                   # For port, pwd.
 import utils           as ut # For access to openSocketsLst[].
 import multiprocessing as mp
+openSocketsLst = []    # Needed for processing close and ks commands.
+#############################################################################
+
+def getActThrds():
+    rspStr = ' Running Threads:\n'
+    for t in th.enumerate():
+        rspStr += '   {}\n'.format(t.name)
+
+    rspStr += '\n Open Sockets:\n'
+    for ii,openS in enumerate(openSocketsLst):
+
+        rspStr+='   Socket {} Object Information \n'.format(ii)
+        rspStr+='     Remote Addr, Port: {}\n'.format(openS['cs'].getpeername())
+        rspStr+='      Local Addr, Port: {}\n'.format(openS['cs'].getsockname())
+        rspStr+='       File descriptor: {}\n'.format(openS['cs'].fileno()     )
+        rspStr+='              Protocol: {}\n'.format(openS['cs'].proto        )
+        rspStr+='                Family: {}\n'.format(openS['cs'].family       )
+        rspStr+='                  Type: {}\n'.format(openS['cs'].type         )
+
+        rspStr+='   Socket {} Address Information \n'.format(ii)
+        rspStr+='               Address: {}\n\n'.format(openS['ca'])
+
+    rspStr += '\n Running Processes:\n'
+    for k,v in cr.procPidDict.items():
+        if v is not None:
+            rspStr += '   {}\n'.format(k)
+    return [rspStr]
 #############################################################################
 
 def getMultiProcSharedDict():
--- utils.py
+++ utils.py
@@ -5,34 +5,6 @@ import subprocess
 import gpiozero
 import timeRoutines    as tr
 
-
-openSocketsLst = []       # Needed for processing close and ks commands.
-#############################################################################
-
-def getActThrds():
-    rspStr = ' Running Threads:\n'
-    for t in th.enumerate():
-        rspStr += '   {}\n'.format(t.name)
-
-    rspStr += '\n Open Sockets:\n'
-    for ii,openS in enumerate(openSocketsLst):
-
-        rspStr+='   Socket {} Object Information \n'.format(ii)
-        rspStr+='     Remote Addr, Port: {}\n'.format(openS['cs'].getpeername())
-        rspStr+='      Local Addr, Port: {}\n'.format(openS['cs'].getsockname())
-        rspStr+='       File descriptor: {}\n'.format(openS['cs'].fileno()     )
-        rspStr+='              Protocol: {}\n'.format(openS['cs'].proto        )
-        rspStr+='                Family: {}\n'.format(openS['cs'].family       )
-        rspStr+='                  Type: {}\n'.format(openS['cs'].type         )
-
-        rspStr+='   Socket {} Address Information \n'.format(ii)
-        rspStr+='               Address: {}\n\n'.format(openS['ca'])
-
-    rspStr += '\n Running Processes:\n'
-    for k,v in cr.procPidDict.items():
-        if v is not None:
-            rspStr += '   {}\n'.format(k)
-    return [rspStr]
 #############################################################################
 
 def getTemp(prnEn = True):
