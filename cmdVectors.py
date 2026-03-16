cf3fba78b13ea8a5cc4f1d700e7ea636c6a2476a
sgarrow <stangarrow@yahoo.com>
2026-03-15 18:12:29 -0700

    "v4.1.8. Just bumping version number"

--- cmdVectors.py
+++ cmdVectors.py
@@ -34,7 +34,7 @@ def dummy():
 
 # Version number of the "app".
 # As opposed to the version number of the "server" which is in fileIO.py
-VER = 'v4.1.7 - 15-Mar-2026'
+VER = 'v4.1.8 - 15-Mar-2026'
 def getVer():
     appVer = VER
     srvVer = fio.VER
